810aa089550697d1fdc499180a7e55cef8f7bf7c
Linh Nguyen <linhnhatmen@gmail.com>
2020-09-28 21:26:24 -0700

    Build a list of questions

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,7 +1,49 @@
-//This is the start of the quiz script
-//Using JQuery
 $(document).ready(function(){
     var startBtn = $("#start-button");
+
+    //List of questions
+    var Questions = [
+        {
+            question: "What is 1 + 1?",
+            answers:{
+                A:"0",
+                B:"1",
+                C:"3",
+                D:"None of the above"
+            },
+            correctAnswer:"D"
+        },
+        {
+            question:"If you drop a ball and a feather at the same time and at the same height in a controlled vacuum on Earth, what will happen?",
+            answers:{
+                A:"the ball hits the ground first",
+                B:"the feather hits the ground first",
+                C:"both the ball and the feather touch the ground at the same time",
+                D:"none of the above"
+            },
+            correctAnswer:"C"
+        },
+        {
+            question:"When was internet invented?",
+            answers:{
+                a:"1980",
+                b:"1983",
+                c:"1985",
+                d:"1987",
+            },
+            correctAnswer:"b"
+        },
+        {
+            question:"What is the name of the hottest pepper in the world?",
+            answers:{
+                a:"Ghost pepper",
+                b:"Trinidad Moruga Scorpion",
+                c:"7 Pot Douglah",
+                d:"Carolina reaper"
+            },
+            correctAnswer:"d"
+        },
+    ];
 
     //Add event listener to the startBtn
     //Once clicked, the initial page is removed
@@ -10,6 +52,6 @@ $(document).ready(function(){
         event.preventDefault();
         $("#initial-page").css("display", "none");
         $("#main-question").css("display", "block");
-        $(".container").css("text-align", "initial");
+        $(".quiz-container").css("text-align", "initial");
     });
 });
